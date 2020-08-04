ed9e3e095cf7bddfdc99b626b214b30822efdb2a
CameronK24 <cskelly15@gmail.com>
2020-08-04 16:52:10 -0600

    worked on making events work

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './events.css';
 
-const Events = () => {
+const Events = props => {
+    
+
     return (
-        <div>Events</div>
+        <div>
+            {props.location.pathname === '/events/new-event'
+            ?
+                <div className='new-event-page'>
+                    <section className='title-section'>
+                        <h1>Event Title:</h1>
+                        <input />
+                    </section>
+                    <section className='date-section'>
+                        <h1>Event Date:</h1>
+                        <input type='date' />
+                    </section>
+                    <section className='time-section'>
+                        <h1>Event Time:</h1>
+                        <input type='time' />
+                    </section>
+                    <section className='details-section'>
+                        <h1>Event Details:</h1>
+                        <textarea></textarea>
+                    </section>
+                </div>
+            :
+                <div className='events-page'>
+                    events
+                </div>
+            }
+        </div>
     )
 }
 
