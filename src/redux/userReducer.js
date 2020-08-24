84096cd2eb774f13e2ea7c599fc3f0c980def666
CameronK24 <cskelly15@gmail.com>
2020-08-23 22:43:53 -0600

    removed some testing code

--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -1,9 +1,9 @@
 const initialState = {
-    userId: '3',
-    email: 'cskelly15@gmail.com',
-    characterName: 'Celestine Spiritfire',
-    avatar: 'https://img2.finalfantasyxiv.com/f/374b43f915652253c3fc6ef461edac03_f0c52cc66e124fc2e641d7c21ab2ce79fc0_96x96.jpg?1595870908',
-    portrait: 'https://img2.finalfantasyxiv.com/f/374b43f915652253c3fc6ef461edac03_f0c52cc66e124fc2e641d7c21ab2ce79fl0_640x873.jpg?1595870908',
+    userId: '',
+    email: '',
+    characterName: '',
+    avatar: '',
+    portrait: '',
 }
 
 const STORE_USER_INFO = 'STORE_USER_INFO';
